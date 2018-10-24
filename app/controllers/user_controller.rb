69385d57a2a7759c7568a7f0ea2faeb56afd8824
mido149z <minhkk149@gmail.com>
2018-10-24 16:52:38 +0700

    update search keywork user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,8 +2,9 @@ class UserController < ApplicationController
 
 	# api GET /users
 	def get_all_users
-		
-		@users = User.all
+
+		# conditions from body request
+		@users = User.where("nickname like ?", "%#{params[:keyword]}%").all
 		render json: {status: 200, message: "Success data!", data: @users}
 	end
 end
